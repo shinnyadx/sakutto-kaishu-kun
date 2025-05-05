4190a60275044d0496c1e6a098dc7d7f37ce7355
shinnyadx <shinnyadx@gmail.com>
2025-05-05 10:41:20 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const mobileMenu = document.getElementById('mobile-menu');
+const navList = document.querySelector('.nav-list');
+
+mobileMenu.addEventListener('click', () => {
+  navList.classList.toggle('active');
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      target.scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+  });
+});
